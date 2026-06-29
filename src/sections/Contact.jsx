938db24e7a16accde173e3ab084e490c3c66d833
zpanev <panev.zoran.te@gmail.com>
2026-06-29 21:50:59 +0200

    reformat

--- src/sections/Contact.jsx
+++ src/sections/Contact.jsx
@@ -1,30 +1,29 @@
 const Contact = () => (
-    <section id="contact" className="contact">
-      <h2>Contact</h2>
+  <section id="contact" className="contact">
+    <h2>Contact</h2>
 
-      {/* <h3>CEO: Ana Ristevska</h3>
+    {/* <h3>CEO: Ana Ristevska</h3>
       <p>Mob: +389 78 391 978</p>
       <br />
       <h3>TUS fractional member: Michaela Dimovska</h3>
       <p>Mob: +1 425 472 8532</p>
       <br /> */}
-      <h3>CEO & CTO: Zoran Panev</h3>
-      <p>Mob: +43 69 010 414 849</p>
-      <br />
+    <h3>CEO & CTO: Zoran Panev</h3>
+    <p>Mob: +43 69 010 414 849</p>
+    <br />
 
-      <div className="contact-info">
-        <p>
-          {/* Office Email: <a href="mailto:javenmk@gmail.com">javenmk@gmail.com</a><br /> */}
-          {/* Contact Email: <a href="mailto:contact@javen.mk">contact@javen.mk</a><br /> */}
-          Contact Email: <a href="mailto:zoran.panev@gmail.com">panev.zoran.te@gmail.com</a><br />
-          {/* LinkedIn: <a href="https://www.linkedin.com/company/javen-it/" target="_blank" rel="noopener noreferrer">JAVEN Web Dev, Graphics, Marketing & IT Education</a><br /> */}
-          {/* LinkedIn: <a href="https://www.linkedin.com/in/ana-ristevska-06897b5b" target="_blank" rel="noopener noreferrer">CEO: Ana Ristevska</a><br /> */}
-          LinkedIn: <a href="https://mk.linkedin.com/in/zoranpanev" target="_blank" rel="noopener noreferrer">CTO: Zoran Panev</a><br />
-          GitHub: <a href="https://github.com/ZokiPokiDev" target="_blank" rel="noopener noreferrer">Zoran Panev Dev</a>
-        </p>
-      </div>
-    </section>
-  );
-  
-  export default Contact;
-  
+    <div className="contact-info">
+      <p>
+        {/* Office Email: <a href="mailto:javenmk@gmail.com">javenmk@gmail.com</a><br /> */}
+        {/* Contact Email: <a href="mailto:contact@javen.mk">contact@javen.mk</a><br /> */}
+        Contact Email: <a href="mailto:zoran.panev@gmail.com">panev.zoran.te@gmail.com</a><br />
+        {/* LinkedIn: <a href="https://www.linkedin.com/company/javen-it/" target="_blank" rel="noopener noreferrer">JAVEN Web Dev, Graphics, Marketing & IT Education</a><br /> */}
+        {/* LinkedIn: <a href="https://www.linkedin.com/in/ana-ristevska-06897b5b" target="_blank" rel="noopener noreferrer">CEO: Ana Ristevska</a><br /> */}
+        LinkedIn: <a href="https://mk.linkedin.com/in/zoranpanev" target="_blank" rel="noopener noreferrer">CTO: Zoran Panev</a><br />
+        GitHub: <a href="https://github.com/ZokiPokiDev" target="_blank" rel="noopener noreferrer">Zoran Panev Dev</a>
+      </p>
+    </div>
+  </section>
+);
+
+export default Contact;
